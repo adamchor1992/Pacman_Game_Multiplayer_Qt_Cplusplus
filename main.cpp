9ad3d54c94fb4cc94c4a36343ee1d8031e546467
Adam Choragwicki <adamchor1992@gmail.com>
2020-10-23 23:33:31 +0200

    add command line testing support

--- main.cpp
+++ main.cpp
@@ -6,16 +6,29 @@ int main(int argc, char *argv[])
 {
     QApplication application(argc, argv);
 
-    ConnectionDialogWindow connectionDialogWindow;
-    connectionDialogWindow.show();
+    /*If no command line arguments were given*/
+    if(argc == 1)
+    {
+        ConnectionDialogWindow connectionDialogWindow;
+        connectionDialogWindow.show();
 
-    /*Divide program into 2 steps*/
-    application.exec();
+        /*Divide program into 2 steps*/
+        application.exec();
 
-    GameWindow gameWindow(nullptr, connectionDialogWindow.GetIpAddress());
-    gameWindow.show();
+        GameWindow gameWindow(nullptr, connectionDialogWindow.GetIpAddress());
+        gameWindow.show();
 
-    application.exec();
+        application.exec();
+    }
+    else
+    {
+        QHostAddress serverAddress("192.168.1.6");
+
+        GameWindow gameWindow(nullptr, serverAddress);
+        gameWindow.show();
+
+        application.exec();
+    }
 
     return 0;
 }
