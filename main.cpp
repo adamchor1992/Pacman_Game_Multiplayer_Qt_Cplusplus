ea9ba51e873aa8882a29f30fa872fee34dfb0dab
Adam Choragwicki <adamchor1992@gmail.com>
2020-10-26 17:07:16 +0100

    add server_ip_address.txt and logging to file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "game_window.h"
 #include "connection_dialog_window.h"
 #include <QApplication>
+#include <fstream>
 
 int main(int argc, char *argv[])
 {
     QApplication application(argc, argv);
+
+    std::ofstream logFile("log.txt", std::ios_base::out | std::ios_base::app);
 
     /*If no command line arguments were given*/
     if(argc == 1)
@@ -22,12 +25,35 @@ int main(int argc, char *argv[])
     }
     else
     {
-        QHostAddress serverAddress("192.168.1.6");
+        std::string inputFileName = "server_ip_address.txt";
 
-        GameWindow gameWindow(nullptr, serverAddress);
-        gameWindow.show();
+        std::ifstream ipAddrInputFile(inputFileName, std::ios_base::in);
 
-        application.exec();
+        if(ipAddrInputFile.is_open())
+        {
+            std::string serverAddress;
+
+            std::getline(ipAddrInputFile, serverAddress);
+
+            if(serverAddress.empty())
+            {
+                logFile << "File " << inputFileName << " is empty" << std::endl << "Application will close now" << std::endl;
+            }
+            else
+            {
+                logFile << "Starting game in TEST mode" << std::endl;
+
+                GameWindow gameWindow(nullptr, QHostAddress(QString::fromStdString(serverAddress)));
+                gameWindow.show();
+
+                application.exec();
+            }
+        }
+        else
+        {
+            logFile << "Could not open " << inputFileName << std::endl << "Application will close now" << std::endl;
+            return 0;
+        }
     }
 
     return 0;
