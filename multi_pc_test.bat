fd359b7b682fc586ee744e716b30c840e848d9eb
Adam Choragwicki <adamchor1992@gmail.com>
2020-11-22 17:15:21 +0100

    modify *.bat

--- multi_pc_test.bat
+++ multi_pc_test.bat
@@ -1,3 +1,3 @@
 cd Client
 cd executable
-Pacman_Client.exe test
+Pacman_Client.exe multi_pc_test_client.txt
--- single_pc_test.bat
+++ single_pc_test.bat
@@ -5,5 +5,5 @@ timeout 1
 
 cd ../../Client/executable
 
-start Pacman_Client.exe client1.txt
-start Pacman_Client.exe client2.txt
+start Pacman_Client.exe single_pc_test_client1.txt
+start Pacman_Client.exe single_pc_test_client2.txt
